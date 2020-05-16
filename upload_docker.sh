0a129e540325f671d54ce3a95b3c497f5f6b20c3
Quentin Cherpozat <61659469+Dennofox@users.noreply.github.com>
2020-05-16 17:48:55 +0200

    use aws complete path so that jenkins can use it

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,7 +1,7 @@
 # This file tags and uploads an image to ECR
 
 # Authenticate to ECR
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 643968575852.dkr.ecr.eu-west-2.amazonaws.com
+/usr/local/bin/aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 643968575852.dkr.ecr.eu-west-2.amazonaws.com
 
 # Tag the new image
 docker tag some-content-nginx:latest 643968575852.dkr.ecr.eu-west-2.amazonaws.com/udacity-captsone:latest
