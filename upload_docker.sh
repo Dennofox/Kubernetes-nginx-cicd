9dd5f5aadcd8267414b5c47f6fa98af68d2da874
Quentin Cherpozat <61659469+Dennofox@users.noreply.github.com>
2020-05-17 17:33:45 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,8 +3,12 @@
 # Authenticate to ECR
 /usr/local/bin/aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 643968575852.dkr.ecr.eu-west-2.amazonaws.com
 
+# Create Tag for new version
+Tag=$(date +"%m-%d-%y-%H-%M-%s")
+export Tag
+
 # Tag the new image
-docker tag nginx-image:latest 643968575852.dkr.ecr.eu-west-2.amazonaws.com/udacity-captsone:latest
+docker tag nginx-image:latest 643968575852.dkr.ecr.eu-west-2.amazonaws.com/udacity-captsone:$Tag
 
 # Push the new image to ECR
-docker push 643968575852.dkr.ecr.eu-west-2.amazonaws.com/udacity-captsone:latest
+docker push 643968575852.dkr.ecr.eu-west-2.amazonaws.com/udacity-captsone:$Tag
