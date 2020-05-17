bdf17aa5348ed8ed623ceba71acd1ce855f86437
Quentin Cherpozat <61659469+Dennofox@users.noreply.github.com>
2020-05-17 15:14:34 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,3 +1,3 @@
 # This file get the latest release and deploys it (rolling deployment)
 
-/usr/bin/kubectl set image deployments/nginx-deployment nginx=643968575852.dkr.ecr.eu-west-2.amazonaws.com/udacity-captsone:latest
+kubectl set image deployments/nginx-deployment nginx=643968575852.dkr.ecr.eu-west-2.amazonaws.com/udacity-captsone:latest
